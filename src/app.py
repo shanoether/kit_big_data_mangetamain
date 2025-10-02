87cfcc347dda8fed6afb21ee024d4830fbd5fd7b
Victor POUSSAIN <victorpoussain@MacBook-Air-de-Vico.local>
2025-10-02 12:19:35 +0200

    streamlit test

--- src/app.py
+++ src/app.py
@@ -1,1 +1,17 @@
-"This is the main application file"
+import streamlit as st
+
+from utils.logger import get_logger
+
+logger = get_logger()
+
+def main():
+    logger.info("this is main")
+
+    test_page = st.Page("frontend/pages/test.py" ,title="Test")
+
+    pg = st.navigation([test_page])
+
+    pg.run()
+
+if __name__ == "__main__":
+    main()
