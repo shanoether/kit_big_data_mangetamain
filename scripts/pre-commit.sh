ad3bf5cb68844d3d33daf17d6867cf76fad2a7d7
lhnguyen <lehoangnguyen510@gmail.com>
2025-10-23 14:51:47 +0200

    add security

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -42,9 +42,6 @@ uv add mkdocs-literate-nav
 
 uv run hatch run docs:serve
 
-uv run coverage run -m pytest tests/
-uv run coverage report
-
 # Then install the precommituv install pre-commit
 uv add pre-commit
 pre-commit install
